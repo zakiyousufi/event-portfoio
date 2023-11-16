be208248efcd8f434c7bde741d260469f89de414
zakiyousufi <ahmadzakiyousufi3@gmail.com>
2023-11-16 19:39:50 +0100

    Create home page

---
+++ src/components/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../style/style.css";
+
+const Home = () => {
+  return (
+    <div className="home w-100">
+      <div className="home-header"> 
+        <h1>Home</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
