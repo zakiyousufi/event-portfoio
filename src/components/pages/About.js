be87d09ddb21eb24d63f6f40c3468c9ed041864e
zakiyousufi <ahmadzakiyousufi3@gmail.com>
2023-11-18 23:19:20 +0100

    Add !importent to the sidebar position fixed

---
+++ src/components/pages/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="d-flex justify-content-center align-items-center flex-column w-100">
+      <h2 className="title mt-2">Get in Touch</h2>
+    </div>
+  );
+}
+
+export default About;
