baf460f1d8041ea59d7e91b2f13e04d02c1af376
zakiyousufi <ahmadzakiyousufi3@gmail.com>
2023-11-18 23:12:38 +0100

    Create routs

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import Sidebar from './components/Sidebar';
 import './App.css';
-import Home from './components/pages/Home';
+import Home from './components/pages/home/Home';
+import About from './components/pages/About';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App d-flex p-5">
-      <Sidebar />
-      <div className='pages'>
-        <Home />
-      </div>
+    <div className="App d-flex">
+      <Router>
+        <Sidebar />
+        <div className='pages'>
+          <Routes>
+              <Route path='/' exact element={<Home />} />
+              <Route path='/about' element={<About />} />
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
